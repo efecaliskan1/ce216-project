bf5f1a9e1088c7dface5258892884c0a62a10efc
ArdaKorkmaz35 <arda.korkmaz.2006@hotmail.com>
2026-03-24 23:32:56 +0300

    Implement DefensiveStrategy

--- src/core/domain/DefensiveStrategy.java
+++ src/core/domain/DefensiveStrategy.java
@@ -1,9 +1,15 @@
-//package tactics;
+package tactics;
 
-//import core.domain.Match;
-//import core.domain.TacticResult;
-//import core.domain.Team;
-//import sports.abstract_.AbstractTacticStrategy;
+import abstracts.AbstractTacticStrategy;
+import core.domain.Match;
+import core.domain.Team;
+import valueobjects.TacticResult;
 
-public class DefensiveStrategy { //extends AbstractTacticStrategy
+public class DefensiveStrategy extends AbstractTacticStrategy {
+    @Override public TacticResult applyTactic(Team team, Match match) {
+        return new TacticResult(0.4f, 0.3f, getFormation());
+    }
+    @Override public String getName()      { return "Defensive"; }
+    @Override public String getFormation() { return "5-4-1"; }
+    @Override public float  getFatigueMultiplier() { return 0.8f; }
 }
