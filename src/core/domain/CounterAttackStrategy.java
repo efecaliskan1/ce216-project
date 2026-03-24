dce5b09bea02578b393cbe807f93b1b26dc3d516
ArdaKorkmaz35 <arda.korkmaz.2006@hotmail.com>
2026-03-24 23:31:49 +0300

    Implement CounterAttackStrategy

--- src/core/domain/CounterAttackStrategy.java
+++ src/core/domain/CounterAttackStrategy.java
@@ -1,9 +1,15 @@
-//package tactics;
+package tactics;
 
-//import core.domain.Match;
-//import core.domain.TacticResult;
-//import core.domain.Team;
-//import sports.abstract_.AbstractTacticStrategy;
+import abstracts.AbstractTacticStrategy;
+import core.domain.Match;
+import core.domain.Team;
+import valueobjects.TacticResult;
 
-public class CounterAttackStrategy { // extends AbstractTacticStrategy
+public class CounterAttackStrategy extends AbstractTacticStrategy {
+    @Override public TacticResult applyTactic(Team team, Match match) {
+        return new TacticResult(0.5f, 0.7f, getFormation());
+    }
+    @Override public String getName()      { return "CounterAttack"; }
+    @Override public String getFormation() { return "4-5-1"; }
+    @Override public float  getFatigueMultiplier() { return 0.9f; }
 }
