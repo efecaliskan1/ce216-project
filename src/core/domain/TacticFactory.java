8535ce33f0e01ed140e5253769493b97bb608f0c
ArdaKorkmaz35 <arda.korkmaz.2006@hotmail.com>
2026-03-24 23:30:17 +0300

    Add TacticFactory implementation
    Implements factory pattern to create different tactic strategies based on input

--- src/core/domain/TacticFactory.java
+++ src/core/domain/TacticFactory.java
@@ -1,6 +1,16 @@
-//package tactics;
+package tactics;
 
-//import interfaces.ITacticStrategy;
+import interfaces.ITacticStrategy;
 
 public class TacticFactory {
+
+    public static ITacticStrategy create(String name) {
+        return switch (name.toLowerCase()) {
+            case "defensive"                  -> new DefensiveStrategy();
+            case "balanced"                   -> new BalancedStrategy();
+            case "highpress", "high_press"    -> new HighPressStrategy();
+            case "counterattack","counter_attack" -> new CounterAttackStrategy();
+            default -> throw new IllegalArgumentException("Unknown tactic: " + name);
+        };
+    }
 }
