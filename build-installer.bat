d16adeb340c894fa1ec810a81f6c8238ba9fb4af
ArdaKorkmaz35 <arda.korkmaz.2006@hotmail.com>
2026-05-10 01:22:31 +0300

    Add files via upload

--- build-installer.bat
+++ build-installer.bat
@@ -2,14 +2,27 @@
 REM ================================================================
 REM  Sport Management System - Windows installer builder
 REM  Requires: JDK 17+ with jpackage (bundled in Oracle / Liberica JDK)
+REM  JAVA_HOME must point to your JDK installation folder.
 REM ================================================================
 
+if "%JAVA_HOME%"=="" (
+  echo ERROR: JAVA_HOME is not set. Please set JAVA_HOME to your JDK install folder.
+  echo Example: setx JAVA_HOME "C:\Program Files\Java\jdk-25"
+  exit /b 1
+)
+
+if not exist "%JAVA_HOME%\bin\jpackage.exe" (
+  echo ERROR: jpackage.exe not found at "%JAVA_HOME%\bin\jpackage.exe"
+  echo Make sure JAVA_HOME points to a JDK 17+ that includes jpackage.
+  exit /b 1
+)
+
 echo [1/2] Building fat jar with Maven...
-call mvn -q clean package
+call mvn -q clean package -DskipTests -Djacoco.skip=true
 if errorlevel 1 goto :err
 
 echo [2/2] Running jpackage...
-jpackage ^
+"%JAVA_HOME%\bin\jpackage" ^
   --type msi ^
   --name "SportManager" ^
   --app-version 3.0.0 ^
